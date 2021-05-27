6748f6579883ca24fc289a9cbe254f7884376ab3
CSRLK <cmiron441@gmail.com>
2021-05-27 01:28:54 -0600

    efra guapo

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,53 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+
+    <div fluid-grow class="p-4 bg-dark">
+        <b-row>
+          <b-col>
+            <b-img thumbnail fluid :src="require('../assets/img/JAVATOOL.jpg')"></b-img>
+          </b-col>
+          <b-col>
+            <b-img thumbnail fluid src="https://i.pinimg.com/originals/34/22/90/3422900cb9e9bd4ce803847129eb6c9f.jpg" alt="Image 2"></b-img>
+          </b-col>
+          <b-col>
+            <b-img thumbnail fluid src="https://picsum.photos/250/250/?image=59" alt="Image 3"></b-img>
+          </b-col>
+        </b-row>
+        </div>
+
+    <div fluid-grow class="bg-secondary">
+        <b-row>
+        
+        </b-row>
+    </div>
+
+    <div  class="bg-dark">
+        
+        <b-carousel
+        id="carousel-fade"
+        style="text-shadow: 0px 0px 2px #000"
+        fade
+        indicators
+        img-width="1024"  
+        img-height="480"
+      >
+        <b-carousel-slide
+          caption="First slide"
+          img-src="https://picsum.photos/1024/480/?image=10"
+        ></b-carousel-slide>
+        <b-carousel-slide
+          caption="Second Slide"
+          img-src="https://picsum.photos/1024/480/?image=12"
+        ></b-carousel-slide>
+        <b-carousel-slide
+          caption="Third Slide"
+          img-src="https://picsum.photos/1024/480/?image=22"
+        ></b-carousel-slide>
+      </b-carousel>
+
+        
+        
+    </div>
+
   </div>
 </template>
