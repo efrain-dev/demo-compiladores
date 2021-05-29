a22c74977484e03fd369b0e8e56b314efd839866
CSRLK <cmiron441@gmail.com>
2021-05-28 22:32:13 -0600

    commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,68 +1,98 @@
 <template>
   <div class="h-full">
 
-
-    
-    
-    <div fluid-grow class="p-4 bg-dark" style="position: absolute; z-index:3; ">
-        <b-row>
+    <div class="p-4 mt-4 d-flex justify-content-center" id="cards">
+       
+        <b-row id="info" >
           <b-col>
             <div>
                 <b-card
-                  title="Card Title"
-                  img-src="https://picsum.photos/600/300/?image=25"
+                  title="Analizador"
+                  :img-src="require('../assets/img/analiz.jpg')"
                   img-alt="Image"
                   img-top
                   tag="article"
-                  style="max-width: 20rem;"
-                  class="mb-2"
+                  style="max-width: 25rem;"
+                  class="mx-5 card"
+                
                 >
                   <b-card-text>
-                    Some quick example text to build on the card title and make up the bulk of the card's content.
+                    Analizador Lexico y Sintactico, ingresa tu codigo y observa como funciona.
                   </b-card-text>
 
-                  <b-button href="#" variant="primary">Go somewhere</b-button>
+                  <b-button to="/analizador" variant="outline-secondary">Probar</b-button>
                 </b-card>
             </div>
           </b-col>
           <b-col>
               <b-card
-                  title="Card Title"
-                  img-src="https://picsum.photos/600/300/?image=25"
+                  title="Implementacion"
+                  :img-src="require('../assets/img/imple.jpg')"
                   img-alt="Image"
                   img-top
                   tag="article"
-                  style="max-width: 20rem;"
-                  class="mb-2"
+                  style="max-width: 25rem;"
+                  class="mx-5 card"
+              
                 >
                   <b-card-text>
-                    Some quick example text to build on the card title and make up the bulk of the card's content.
+                    Conoce como comenzar, la manera de la Implementacion
                   </b-card-text>
 
-                  <b-button href="#" variant="primary">Go somewhere</b-button>
+                  <b-button to="/implementacion" variant="outline-secondary">Comenzar</b-button>
                 </b-card>
             
           </b-col>
           <b-col>
               <b-card
-                  title="Card Title"
-                  img-src="https://picsum.photos/600/300/?image=25"
+                  title="Quienes Somos"
+                  :img-src="require('../assets/img/somos.jpg')"
                   img-alt="Image"
                   img-top
                   tag="article"
-                  style="max-width: 20rem;"
-                  class="mb-2"
+                  style="max-width: 25rem;"
+                  class="mx-5 card"
+                 
                 >
                   <b-card-text>
-                    Some quick example text to build on the card title and make up the bulk of the card's content.
+                    Conoce mas de nosotros, toda la iformacion que necesitas saber, la encontraras aqui. 
                   </b-card-text>
 
-                  <b-button href="#" variant="primary">Go somewhere</b-button>
+                  <b-button to="/about" variant="outline-secondary">Conocenos</b-button>
                 </b-card>
            
           </b-col>
-        </b-row>
+ 
+        </b-row>  
+
         </div>
+        <div class="p-4 d-flex justify-content-center" id="cards">
+       
+        <b-row id="info" >
+
+          <b-col>
+          <b-card
+                  title="Cuenta"
+                  :img-src="require('../assets/img/cuenta.jpg')"
+                  img-alt="Image"
+                  img-top
+                  tag="article"
+                  style="max-width: 25rem;"
+                  class="mx-5 card"
+                 
+                >
+                  <b-card-text>
+                    Chequea tu perfil 
+                  </b-card-text>
+
+                  <b-button to="/cuenta" variant="outline-secondary">Conocenos</b-button>
+                </b-card>
+        </b-col>
+        </b-row>  
+
+        </div>
+
+        
 
     
 
@@ -83,5 +113,27 @@ export default {
 </script>
 
 <style>
+#cards{
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  /* background-color: #8c8c8c; */
+  
+}
+
+#info{
+  
+}
+
+.card {
+            overflow: hidden;
+            transition: all 0.25s;
+        }
+
+        .card:hover {
+            transform: translateY(-15px);
+            box-shadow: 0 12px 16px rgba(0, 0, 0, 0.2);
+        }
+
 
 </style>
